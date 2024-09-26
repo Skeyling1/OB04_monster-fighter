2777d3c91ff75151378a481ebe7c5e151c1b26db
Skeyling1 <skeyling1@gmail.com>
2024-09-26 21:20:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-from abc import ABC, abstractmethod
-
+# Есть класс Fighter, представляющий бойца.
+# Есть класс Monster, представляющий монстра.
+# Игрок управляет бойцом и может выбирать для него одно из вооружений для боя.
 
 class Fighter:
-    def __init__(self):
-        pass
+    def __init__(self, Weapon):
+        self.weapon = Weapon()
+
+    def changeWeapon(self, Weapon):
+        self.weapon = Weapon()
 
 
 class Monster:
-    def __init__(self):
+    pass
+
+from abc import ABC, abstractmethod
+
+class Weapon(ABC):
+    @abstractmethod
+    def attack(self):
         pass
 
-class
+class Sword(Weapon):
+    def attack(self):
+        print("\nБоец выбирает меч.")
+        print("Боец наносит удар мечом.")
+        print("Монстр побежден!")
+
+class Bow(Weapon):
+    def attack(self):
+        print("\nБоец выбирает лук.")
+        print("Боец разит из лука.")
+        print("Монстр побежден!")
+
+
+figh1 = Fighter(Sword)
+
+figh1.weapon.attack()
+
+figh1.changeWeapon(Bow)
+
+figh1.weapon.attack()
+
